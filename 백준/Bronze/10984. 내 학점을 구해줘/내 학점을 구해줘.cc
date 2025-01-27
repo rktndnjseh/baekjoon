7b8ac2feaf94930681960e222b4b13c35e1a7c97
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2025-01-27 18:39:17 +0900

    [Bronze III] Title: 내 학점을 구해줘, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10984. 내 학점을 구해줘/내 학점을 구해줘.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int T, N, C, sum;
+    float G, GPA;
+    cin >> T;
+    while(T-->0){
+        cin >> N;
+        sum = 0;
+        GPA = 0;
+        for(int i = 0; i < N; i++){
+            cin >> C;
+            cin >> G;
+            sum += C;
+            GPA += C * G;
+        }
+        cout << sum <<" "<< fixed << setprecision(1) << GPA/sum<<"\n";
+    }
+}
