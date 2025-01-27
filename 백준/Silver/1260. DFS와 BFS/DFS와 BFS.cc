22c0923b761140cedb3f13f6b759d2c67c983360
rktndnjseh <150515419+rktndnjseh@users.noreply.github.com>
2025-01-27 17:06:16 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -45,7 +45,7 @@ int main(){
         graph[u].push_back(v);
         graph[v].push_back(u);    
     }   
-    for(int i=1;i<=N;i++){
+    for(int i=0;i<=N;i++){
         sort(graph[i].begin(), graph[i].end());
     }
     dfs(V);
